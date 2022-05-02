31961b757790f1491f4b20d383e3cb0f85c735d6
wwwdeveloperman <wwwdeveloperman@gmail.com>
2022-05-01 23:35:40 -0300

    summary :  to min 41. lista los cursos. display de botones.
    descripcion:

--- RAZOR_NET6_VS2022/SOL_CRUDNET6RAZORVS2022/CRUDNET6RAZORVS2022/Pages/Cursos/Index.cshtml.cs
+++ RAZOR_NET6_VS2022/SOL_CRUDNET6RAZORVS2022/CRUDNET6RAZORVS2022/Pages/Cursos/Index.cshtml.cs
@@ -1,12 +1,32 @@
+using CRUDNET6RAZORVS2022.Datos;
+using CRUDNET6RAZORVS2022.Modelos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace CRUDNET6RAZORVS2022.Pages.Cursos
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        //----------------------------
+        private readonly  DB _contexto;
+        public IndexModel(DB contexto)
         {
+            _contexto = contexto;
         }
-    }
+        //----------------------------
+        
+        
+        // Obtener una lista de Cursos p usar en el html
+        public IEnumerable<Curso> Cursos { get; set; }
+        public async Task OnGet()
+        {
+            Cursos = await _contexto.Curso.ToListAsync();
+        }
+
+
+
+
+
+    }//class 
 }
